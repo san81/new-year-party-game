1efcdfa6f5c655f5760720e74d3fd5252207522d
sgandhe <santhoshgandhe@Santhoshs-MacBook-Pro.local>
2025-01-01 19:42:59 -0800

    additinal content

--- NewYearParty/scripts/itemsJsonData.js
+++ NewYearParty/scripts/itemsJsonData.js
@@ -1,55 +1,5 @@
 // JSON list with categories
 
-const originalJsonData = {
-    "logo": [
-        "Amazon.png",
-        "Apple.png",
-        "McDonalds.png",
-        "MrBeast.png" 
-    ],
-    "kitchenObject": [
-        "Fork.png",
-        "Spoon.png",
-        "Blender.png",
-        "Napkin.png",
-        "Glass.png",
-        "RollingPin.png",
-        "Knife.png",
-        "Spatula.png",
-        "Shredder.png",
-        "Pan.png",
-        "HandBlender.png"
-    ],
-    "animals": [
-        "Cat.png",
-        "Cheetah.png",
-        "Chimpanzee.png",
-        "Donkey.png",
-        "Fox.png",
-        "Hippopotamus.png",
-        "Koala.png",
-        "Lama.png",
-        "Penguin.png",
-        "PolarBear.png",
-        "Rabbit.png",
-        "Rhinoceros.png",
-        "Sheep.png",
-        "Tiger.png",
-        "Zebra.png",
-        "pig.png"
-    ],
-    "movieNameFromHerosPic": [
-        "Lion.png"
-    ],
-    "movieNameFromPoster": [
-        "Lion.png"
-    ],
-    "seeds": [
-        "Lion.png"
-    ]
-
-};
-
 
 function shuffleObjectArrays(obj) {
     const shuffledObj = {};
